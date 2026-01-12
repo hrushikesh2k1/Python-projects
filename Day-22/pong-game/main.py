b4fec54745ee85c832727cba37c52329424b11a0
Hrushikesh Boora <hrushikeshboora@gmail.com>
2026-01-12 16:40:23 +0530

    Create main.py

---
+++ Day-22/pong-game/main.py
@@ -0,0 +1,64 @@
+from turtle import Turtle, Screen
+import time
+from paddle import Paddle
+from ball import Ball
+from score_board import Score
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+ball = Ball()
+score = Score()
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if(ball.ycor() > 280 or ball.ycor() < -280):
+        ball.bounce_y()
+    
+    # detect collision with paddle
+    if(ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    # Detect when right paddle misses
+    if(ball.xcor() > 380):
+        ball.reset_position()
+        score.l_point()
+    
+    # Detect when left paddle missed
+    if(ball.xcor() < -380):
+        ball.reset_position()
+        score.r_point()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
